f26840373f441a30d31d035548e6117888b39c05
betobrandaojr <59041231+betobrandaojr@users.noreply.github.com>
2024-03-07 00:00:08 -0300

    first commit

---
+++ src/main/java/com/brandao/os/domain/Client.java
@@ -0,0 +1,15 @@
+package com.brandao.os.domain;
+
+public class Client extends Pessoa {
+
+	public Client() {
+		super();
+
+	}
+
+	public Client(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+
+	}
+
+}
